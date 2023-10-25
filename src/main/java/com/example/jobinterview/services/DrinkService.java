5e4076660c2ef23a1ccb3bed038d631226546962
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 15:02:46 +0200

    Create a method to get drinks

--- src/main/java/com/example/jobinterview/services/DrinkService.java
+++ src/main/java/com/example/jobinterview/services/DrinkService.java
@@ -1,8 +1,22 @@
 package com.example.jobinterview.services;
 
+import com.example.jobinterview.models.Drink;
+import com.example.jobinterview.repositories.DrinkRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class DrinkService {
+import java.util.List;
 
+@Service
+@AllArgsConstructor
+public class DrinkService {
+    DrinkRepository drinkRepository;
+
+    public List<Drink> getAllDrinks() {
+        List<Drink> allDrinks = drinkRepository.findAll();
+        if (allDrinks.isEmpty()) {
+            throw new Error("There is not any drink in our Drink Menu yet.");
+        }
+        return allDrinks;
+    }
 }

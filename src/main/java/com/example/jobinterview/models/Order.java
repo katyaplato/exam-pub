4f7312c00708d044515e664283cb169d5ad144c2
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 12:33:24 +0200

    Create an Order entity

--- src/main/java/com/example/jobinterview/models/Order.java
+++ src/main/java/com/example/jobinterview/models/Order.java
@@ -1,4 +1,19 @@
 package com.example.jobinterview.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "orders")
+@Data
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private int amount;
+    private double price;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }

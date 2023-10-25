5da26fc09a8edd32fbe365f940b4093d5e6766e2
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 14:26:15 +0200

    Create endpoint get all users

--- src/main/java/com/example/jobinterview/controllers/ApiController.java
+++ src/main/java/com/example/jobinterview/controllers/ApiController.java
@@ -1,16 +1,22 @@
 package com.example.jobinterview.controllers;
 
-import org.apache.catalina.User;
+import com.example.jobinterview.models.User;
+import com.example.jobinterview.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+    UserService userService;
+
     @GetMapping("/users")
-    public Iterable<User> getAllUsers(){
-        return;
+    public List<User> getAllUsers() {
+        return  userService.getAllUsers();
     }
 
 }

93baa01661c968a8ba20a2bf8103d32e1ab424b7
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 12:25:54 +0200

    Create a User entity

---
+++ src/main/java/com/example/jobinterview/models/User.java
@@ -0,0 +1,25 @@
+package com.example.jobinterview.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private boolean isActive;
+    private boolean isAdult;
+    private double pocket;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+}

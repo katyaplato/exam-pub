169fa51c7a6115e99886030c36edd1c85cc75f2b
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 21:41:15 +0200

    update fields

--- src/main/java/com/example/jobinterview/models/User.java
+++ src/main/java/com/example/jobinterview/models/User.java
@@ -13,6 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String password;
     private boolean isActive;
     private boolean isAdult;
     private double pocket;

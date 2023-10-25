b0706f2d49c3e883777b026bc8cd52460807a4e1
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-25 12:34:09 +0200

    update

--- src/main/java/com/example/jobinterview/models/User.java
+++ src/main/java/com/example/jobinterview/models/User.java
@@ -1,22 +1,16 @@
 package com.example.jobinterview.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private boolean isActive;

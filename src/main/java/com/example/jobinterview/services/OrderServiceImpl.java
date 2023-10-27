290b17f2c1b0f00136819e0d50d3539c34494b2f
Kateryna_Platonova <katyaplato@gmail.com>
2023-10-27 12:21:44 +0200

    add error handling

--- src/main/java/com/example/jobinterview/services/OrderServiceImpl.java
+++ src/main/java/com/example/jobinterview/services/OrderServiceImpl.java
@@ -26,99 +26,112 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public ResponseEntity<List<SummaryAllDTO>> getSummaryAll() {
-        List<Order> allOrders = orderRepository.findAll();
-        List<SummaryAllDTO> summary = new ArrayList<>();
+        try {
+            List<Order> allOrders = orderRepository.findAll();
+            List<SummaryAllDTO> summary = new ArrayList<>();
 
-        for (Order order : allOrders) {
-            Product product = order.getProduct();
+            for (Order order : allOrders) {
+                Product product = order.getProduct();
 
-            SummaryAllDTO summaryAllDTO = SummaryAllDTO
-                    .builder()
-                    .productName(product.getProductName())
-                    .amount(order.getAmount())
-                    .unitPrice(product.getPrice())
-                    .summaryPrice(order.getPrice())
-                    .build();
+                SummaryAllDTO summaryAllDTO = SummaryAllDTO
+                        .builder()
+                        .productName(product.getProductName())
+                        .amount(order.getAmount())
+                        .unitPrice(product.getPrice())
+                        .summaryPrice(order.getPrice())
+                        .build();
 
-            summary.add(summaryAllDTO);
+                summary.add(summaryAllDTO);
+            }
+            return ResponseEntity.ok(summary);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok(summary);
     }
 
     @Override
     public ResponseEntity<List<SummaryProductDTO>> getSummaryProduct() {
-        List<Order> allOrders = orderRepository.findAll();
-        List<SummaryProductDTO> productSummary = new ArrayList<>();
+        try {
+            List<Order> allOrders = orderRepository.findAll();
+            List<SummaryProductDTO> productSummary = new ArrayList<>();
 
-        for (Order order : allOrders) {
-            User user = order.getUser();
-            SummaryProductDTO summaryProductDTO = SummaryProductDTO
-                    .builder()
-                    .userId(user.getId())
-                    .amount(order.getAmount())
-                    .price(order.getPrice())
-                    .build();
+            for (Order order : allOrders) {
+                User user = order.getUser();
+                SummaryProductDTO summaryProductDTO = SummaryProductDTO
+                        .builder()
+                        .userId(user.getId())
+                        .amount(order.getAmount())
+                        .price(order.getPrice())
+                        .build();
 
-            productSummary.add(summaryProductDTO);
+                productSummary.add(summaryProductDTO);
+            }
+            return ResponseEntity.ok(productSummary);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok(productSummary);
     }
 
     @Override
     public ResponseEntity<List<SummaryUserDTO>> getSummaryUser() {
-        List<Order> allOrders = orderRepository.findAll();
-        List<SummaryUserDTO> summaryUser = new ArrayList<>();
+        try {
+            List<Order> allOrders = orderRepository.findAll();
+            List<SummaryUserDTO> summaryUser = new ArrayList<>();
 
-        for (Order order : allOrders) {
-            User user = order.getUser();
-            Product product = order.getProduct();
+            for (Order order : allOrders) {
+                User user = order.getUser();
+                Product product = order.getProduct();
 
-            SummaryUserDTO summaryUserDTO = SummaryUserDTO
-                    .builder()
-                    .userId(user.getId())
-                    .productName(product.getProductName())
-                    .price(order.getPrice())
-                    .build();
-            summaryUser.add(summaryUserDTO);
-        }
-        return ResponseEntity.ok(summaryUser);
-    }
-
-    @Override
-    public String buyProduct(Long userId, Long productId, int amount) {
-
-        Optional<User> maybeUser = userRepository.findById(userId);
-        Optional<Product> maybeProduct = productRepository.findById(productId);
-
-        if (maybeUser.isEmpty()) {
-            throw new Error("A User with such ID does not exist.");
-        } else if (maybeProduct.isEmpty()) {
-            throw new Error("A drink with such ID does not exist.");
-        }
-
-        Product product = maybeProduct.get();
-        User user = maybeUser.get();
-
-        if (product.isForAdult()) {
-            if (!user.isAdult()) {
-                throw new Error("You are not allowed to drink alcohol!");
-            } else if (user.getPocket() < (product.getPrice() * amount)) {
-                throw new Error("You cannot afford it.");
+                SummaryUserDTO summaryUserDTO = SummaryUserDTO
+                        .builder()
+                        .userId(user.getId())
+                        .productName(product.getProductName())
+                        .price(order.getPrice())
+                        .build();
+                summaryUser.add(summaryUserDTO);
             }
+            return ResponseEntity.ok(summaryUser);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
-
-        user.setPocket(user.getPocket() - product.getPrice());
-
-        Order order = Order
-                .builder()
-                .product(product)
-                .amount(amount)
-                .price(product.getPrice() * amount)
-                .user(user)
-                .build();
-
-        user.getOrders().add(order);
-
-        return "Have a nice evening.";
     }
-}
+
+
+        @Override
+        public String buyProduct (Long userId, Long productId,int amount){
+
+            Optional<User> maybeUser = userRepository.findById(userId);
+            Optional<Product> maybeProduct = productRepository.findById(productId);
+
+            if (maybeUser.isEmpty()) {
+                throw new Error("A User with such ID does not exist.");
+            } else if (maybeProduct.isEmpty()) {
+                throw new Error("A drink with such ID does not exist.");
+            }
+
+            Product product = maybeProduct.get();
+            User user = maybeUser.get();
+
+            if (product.isForAdult()) {
+                if (!user.isAdult()) {
+                    throw new Error("You are not allowed to drink alcohol!");
+                } else if (user.getPocket() < (product.getPrice() * amount)) {
+                    throw new Error("You cannot afford it.");
+                }
+            }
+
+            user.setPocket(user.getPocket() - product.getPrice());
+
+            Order order = Order
+                    .builder()
+                    .product(product)
+                    .amount(amount)
+                    .price(product.getPrice() * amount)
+                    .user(user)
+                    .build();
+
+            user.getOrders().add(order);
+
+            return "Have a nice evening.";
+        }
+    }
